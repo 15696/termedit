626077d7d63ed70bc57a30b941087c031ea462d9
BuildTools <unconfigured@null.spigotmc.org>
2021-05-05 08:54:16 -0600

    idk

--- termedit/__main__.py
+++ termedit/__main__.py
@@ -1,1 +1,27 @@
 import curses
+
+HEIGHT = 5
+WIDTH = 40
+BEGIN_X = 20
+BEGIN_Y = 7
+
+stdscr = curses.initscr()
+stdscr.keypad(True)
+
+curses.noecho()
+curses.cbreak()
+
+def close(stdscr):
+	stdscr.keypad(False)
+	curses.nocbreak()
+	curses.echo()
+	
+	curses.endwin()
+
+def main(stdscr):
+	stdscr.clear()
+	
+	stdscr.addstr(0, 0, "hi", curses.A_BLINK)	
+	stdscr.refresh()
+
+curses.wrapper(main)
